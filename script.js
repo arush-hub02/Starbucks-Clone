8b8def981a4a8e3d9f27a5a2233e0df49f49d4d7
arush-hub02 <80334097+arush-hub02@users.noreply.github.com>
2024-03-26 16:44:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// let navContents = document.querySelector(".navcont");
+
+// navContents.addEventListener("click", function () {
+//   navContents.style.borderWidth = 3;
+//   navContents.style.borderColor = red;
+// });
